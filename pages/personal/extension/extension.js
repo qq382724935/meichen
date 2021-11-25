c45c658e13b495314e31270a830886eeb2f99ad2
qq382724935 <382724935@qq.com>
2021-11-25 14:43:05 +0800

    1

--- pages/personal/extension/extension.js
+++ pages/personal/extension/extension.js
@@ -2,30 +2,30 @@
  * @Author: 刘利军
  * @Date: 2021-09-11 14:42:24
  * @LastEditors: 刘利军
- * @LastEditTime: 2021-11-11 17:47:23
+ * @LastEditTime: 2021-11-25 14:41:43
  * @Description:
  * @PageName:
  */
 // pages/personal/extension/extension.js
 
-
 var app = getApp();
 var request = app.request;
-
 
 Page({
   /**
    * 页面的初始数据
    */
-  data: {},
+  data: { qrcode: "" },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function () {
+    const self = this;
     wx.showLoading({ mask: true });
     request.get("/user/promote-code", {
       success: function (res) {
+        self.setData({ qrcode: res.data.result.file });
       },
       complete: function () {
         wx.hideLoading();
